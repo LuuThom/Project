638023720d47792524df82fd3fee680fb362e936
Luu Thom <baobinhsmile95@gmail.com>
2018-04-22 06:54:56 +0000

    finish part 7

--- Gemfile
+++ Gemfile
@@ -7,7 +7,10 @@ ruby '2.3.0'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'devise'
 gem 'carrierwave'
+gem 'mini_magick', '3.8.0'
+gem 'rb-readline'
 gem 'net-ssh'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
   get 'pages/info'
-    root 'ideas#index'
+  root 'ideas#index'
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
